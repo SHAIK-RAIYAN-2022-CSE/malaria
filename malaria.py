3d1f59b2557ff6777686197e34cc6aa3185bb697
SHAIK-RAIYAN-2022-CSE <99220041364@klu.ac.in>
2024-10-18 17:16:29 +0530

    Update malaria.py

--- malaria.py
+++ malaria.py
@@ -2,63 +2,58 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-
-# loading the saved models
-
+# Load the saved model
 malaria = pickle.load(open('malaria_model.sav', 'rb'))
-# page title
+
+# Page title
 st.title('Malaria Prediction using ML')
 
-
-# getting the input data from the user
-col1, col2, col3,col4, col5 = st.columns(5)
+# Input data
+col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
-    Diagnosis = st.text_input('Region')
-    
+    region = st.text_input('Region')
+
 with col2:
-    Radius_mean = st.text_input('Year')
+    year = st.text_input('Year')
 
 with col3:
-    Texture_mean = st.text_input('Month')
-    
+    month = st.text_input('Month')
+
 with col4:
-    Perimeter_mean = st.text_input('Temperature Above Avg')
-    
+    temperature_above_avg = st.text_input('Temperature Above Avg')
+
 with col5:
-    Area_mean = st.text_input('High_Rainfall')
+    high_rainfall = st.text_input('High Rainfall')
 
 with col1:
-    Smoothness_mean = st.text_input('High_Humidity')
-    
+    high_humidity = st.text_input('High Humidity')
+
 with col2:
-    Compactness_mean = st.text_input('High_Population_Density')
+    high_population_density = st.text_input('High Population Density')
 
 with col3:
-    Contactivity_mean = st.text_input('Malaria_Outbreak')
-    
+    malaria_outbreak = st.text_input('Malaria Outbreak')
+
 with col4:
-    Concave_points_mean = st.text_input('Health_Facilities_Adequate')
-  
+    health_facilities_adequate = st.text_input('Health Facilities Adequate')
+
 with col5:
-    Area_mean = st.text_input('Vaccination_Rate_High')
+    vaccination_rate_high = st.text_input('Vaccination Rate High')
 
 with col1:
-    Smoothness_mean = st.text_input('Mosquito_Net_Coverage_High')
+    mosquito_net_coverage_high = st.text_input('Mosquito Net Coverage High')
 
-
-# code for Prediction
+# Prediction result
 malaria_diagnosis = ''
 
-# creating a button for Prediction
-
+# Prediction button
 if st.button('Malaria Test Button'):
-    malaria_prediction = malaria.predict([[Temperature_Above_Avg,High_Rainfall,High_Humidity,High_Population_Density,Malaria_Outbreak,Insecticide_Use,Health_Facilities_Adequate,Vaccination_Rate_High,Mosquito_Net_Coverage_High
-]])
-    
-    if (malaria_prediction[0] == 1):
-      malaria_diagnosis = 'The person is Effected with Malaria'
+    malaria_prediction = malaria.predict([[temperature_above_avg, high_rainfall, high_humidity, high_population_density, malaria_outbreak, health_facilities_adequate, vaccination_rate_high, mosquito_net_coverage_high]])
+
+    if malaria_prediction[0] == 1:
+        malaria_diagnosis = 'The person is affected with Malaria'
     else:
-      malaria_diagnosis = 'The person is NOT Effected with Malaria'
-    
+        malaria_diagnosis = 'The person is NOT affected with Malaria'
+
 st.success(malaria_diagnosis)
